17a244fbe1beb17bc8a2d5da6c8bd1e9ed307874
aidanliddiard <aidanliddiard@gmail.com>
2022-02-08 17:55:54 -0800

    aadding css

--- states/states.js
+++ states/states.js
@@ -1,9 +1,16 @@
 import { states } from '../states-data.js';
 import { findById } from '../utils.js';
+import { renderState } from '../render.js';
 
 const params = new URLSearchParams(window.location.search);
 
 const state = findById(params.get('id'), states);
+console.log(params.get('id'));
+console.log(state);
+console.log(window.location.search);
 
-const name = document.getElementById('name');
-name.textContent = state.name;
+
+const statesList = document.getElementById('states');
+
+const div = renderState(state);
+statesList.append(div);
