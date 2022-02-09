806d20f3716a812520b84d70fddc0693891dcd7a
aidanliddiard <aidanliddiard@gmail.com>
2022-02-09 14:56:48 -0800

    adding images and fixing test

--- render.js
+++ render.js
@@ -1,24 +1,52 @@
 export function renderState(object) {
-    const div = document.createElement('div');
-    div.classList.add('states');
+    const divContainter = document.createElement('div');
+    divContainter.classList.add('container');
+    //state
+    const divState = document.createElement('div');
+    divState.classList.add('State');
+    const h1 = document.createElement('h1');
+    h1.textContent = object.name;
     const h3 = document.createElement('h3');
-    h3.textContent = object.name;
-    const ul = document.createElement('ul');
-    //flower
-    const liFlower = document.createElement('li');
-    liFlower.textContent = `Flower: ${object.flower}`;
+    h3.textContent = object.nickname;
+    divState.append(h1, h3);
     //animal
-    const liAnimal = document.createElement('li');
-    liAnimal.textContent = `Animal: ${object.animal}`;
+    const divAnimal = document.createElement('div');
+    divAnimal.classList.add('Animal');
+    const imgAnimal = document.createElement('img');
+    imgAnimal.classList.add('img');
+    imgAnimal.src = `${object.animalImg}`;
+    const pAnimal = document.createElement('p');
+    pAnimal.textContent = `State Animal: ${object.animal}`;
+    divAnimal.append(imgAnimal, pAnimal,);
+    //flower
+    const divFlower = document.createElement('div');
+    divFlower.classList.add('flower');
+    const imgFlower = document.createElement('img');
+    imgFlower.classList.add('img');
+    imgFlower.src = `${object.flowerImg}`;
+    const pFlower = document.createElement('p');
+    pFlower.textContent = `State Flower: ${object.flower}`;
+    divFlower.append(imgFlower, pFlower);
     //fruit
-    const liFruit = document.createElement('li');
-    liFruit.textContent = `Fruit: ${object.Fruit}`;
+    const divFruit = document.createElement('div');
+    divFruit.classList.add('fruit');
+    const imgFruit = document.createElement('img');
+    imgFruit.classList.add('img');
+    imgFruit.src = `${object.fruitImg}`;
+    const pFruit = document.createElement('p');
+    pFruit.textContent = `State Fruit: ${object.Fruit}`;
+    divFruit.append(imgFruit, pFruit);
     //tree
-    const liTree = document.createElement('li');
-    liTree.textContent = `Tree: ${object.Tree}`;
-    ul.append(liFlower, liAnimal, liFruit, liTree);
-    div.append(h3, ul);
-    return div;
+    const divTree = document.createElement('div');
+    divTree.classList.add('tree');
+    const imgTree = document.createElement('img');
+    imgTree.classList.add('img');
+    imgTree.src = `${object.treeImg}`;
+    const pTree = document.createElement('p');
+    pTree.textContent = `State Tree: ${object.Tree}`;
+    divTree.append(imgTree, pTree);
+    divContainter.append(divState, divAnimal, divFlower, divFruit, divTree);
+    return divContainter;
 }
 
 export function renderStateImg(object) {
--- test/utils.test.js
+++ test/utils.test.js
@@ -5,16 +5,23 @@ const test = QUnit.test;
 test('testing state function', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = `<div class="states"><h3>Oregon</h3><ul><li>Flower: Oregon-grape</li><li>Animal: American beaver</li><li>Fruit: Pear</li><li>Tree: Douglas-fir</li></ul></div>`;
+    const expected = `<div class="container"><div class="State"><h1>Oregon</h1><h3>The Beaver State</h3></div><div class="Animal"><img class="img" src="../assets/francesco-ungaro-cHVKmOwd0A8-unsplash.jpg"><p>State Animal: American beaver</p></div><div class="flower"><img class="img" src="../assets/oregon-state-flower-gcdc3b0078_1920.jpg"><p>State Flower: Oregon Grape</p></div><div class="fruit"><img class="img" src="../assets/pexels-mali-maeder-568471.jpg"><p>State Fruit: Pear</p></div><div class="tree"><img class="img" src="../assets/douglas-fir-g2ee540245_1920.jpg"><p>State Tree: Douglas-fir</p></div></div>`;
     
     //Act 
     // Call the function you're testing and set the result to a const
     const actual = renderState({
+        id: '1',
         name: 'Oregon',
+        nickname: 'The Beaver State',
+        img: 'assets/icons8-oregon-100.png',
         animal: 'American beaver',
-        flower: 'Oregon-grape',
+        animalImg: '../assets/francesco-ungaro-cHVKmOwd0A8-unsplash.jpg',
+        flower: 'Oregon Grape',
+        flowerImg: '../assets/oregon-state-flower-gcdc3b0078_1920.jpg',
         Fruit: 'Pear',
+        fruitImg: '../assets/pexels-mali-maeder-568471.jpg',
         Tree: 'Douglas-fir',
+        treeImg: '../assets/douglas-fir-g2ee540245_1920.jpg',
     });
 
     //Expect
